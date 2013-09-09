e265b68cfaa5acb5aaef734841c27b83933e39d9
Rich Wareham <rjw57@cam.ac.uk>
2013-09-09 15:11:18 +0100

    flesh out cheeseshop metadata for 0.1 release

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author = 'Rich Wareham',
     author_email = 'rjw57@cantab.net',
     description = 'Renormalise a set of images on the command line',
+    url = 'https://github.com/rjw57/renormimg',
     long_description = open('README.rst').read(),
     license = 'MIT',
     packages = find_packages(),
@@ -15,6 +16,19 @@ setup(
         'renormimg = renormimg.tool:main',
     ] },
     install_requires = [ 'docopt', 'numpy', 'matplotlib', 'PIL', ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Utilities',
+    ],
 )
 
 # vim:sw=4:sts=4:et
